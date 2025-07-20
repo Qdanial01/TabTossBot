116dec61f8e0c033b27ea974c03125780faa9689
Qamarrul Danial <q.qamarrul@gmail.com>
2025-07-20 15:31:57 +0800

    Version 1.0
    List is based of personal group and therefore not ready for dynamic usage from different groups.

---
+++ main.py
@@ -0,0 +1,56 @@
+from typing import Final
+from random import choice
+import os
+from dotenv import load_dotenv
+from telegram import Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import CallbackQueryHandler
+import json
+
+#from response import handle_response
+
+#Load json data from replies.json
+with open('replies.json', 'r', encoding='utf-8') as file:
+    replies = json.load(file)
+
+#.env file
+load_dotenv()
+Bot_Token: Final[str] = os.getenv('Bot_Token')
+Bot_Username: Final = '@TabToss_Bot'
+
+#KGC List
+kgcList = ['Don', 'Joel', 'Imam', 'Qam']
+
+
+
+#Commmands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Hello! Ready to pay the bill? (hopefully it\'s not you this time!)')
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('use the toss command to see who\'s paying first!')
+
+async def toss_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    personPaying = choice(kgcList)
+    reply = choice(replies)
+    finalTab = reply.format(personPaying)
+    await update.message.reply_text(finalTab)
+
+
+
+#main function to start the bot
+if __name__ == '__main__':
+    print('Starting bot...')
+    app = Application.builder().token(Bot_Token).build()
+
+    #Commands
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('toss', toss_command))
+
+    #Messages
+
+#Polls the text (ensures previous handlers run)
+    print('Polling.....')
+    app.run_polling(poll_interval=3)
